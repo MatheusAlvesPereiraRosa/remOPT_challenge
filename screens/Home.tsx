bbbb7598b39c704de1cd9e27a40525d715bcf637
Matheus Alves <matpr.a98@gmail.com>
2023-08-03 12:08:53 -0300

    Iniciando implementação da primeira tela

--- screens/Home.tsx
+++ screens/Home.tsx
@@ -1,26 +1,67 @@
 import React from 'react';
-import {StyleSheet, Text, View, Button} from 'react-native';
+import {
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity,
+  ImageBackground,
+} from 'react-native';
 
 export const Home = () => {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Matheus Alves Pereira Rosa</Text>
-      <Text style={styles.text}>Pokemon Go</Text>
-      <Button title="Ler QR code" />
+      <ImageBackground
+        source={require('../assets/pokemon-go-wallpaper.jpg')}
+        style={styles.imageBackground}>
+        <View style={styles.infoContainer}>
+          <Text style={styles.label}>Matheus Alves Pereira Rosa</Text>
+          <View style={styles.buttonContainer}>
+            <TouchableOpacity style={styles.button}>
+              <Text style={styles.buttonText}>Scannear QR Code</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+      </ImageBackground>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
+    flexDirection: 'column',
+    flex: 1,
+  },
+  imageBackground: {
+    flex: 1,
+    resizeMode: 'cover',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  infoContainer: {
     display: 'flex',
-    justifyContent: 'space-evenly',
+    justifyContent: 'space-between',
     alignContent: 'center',
     alignItems: 'center',
     flex: 1,
   },
-  text: {
+  label: {
     color: '#FF0000',
     textAlign: 'center',
+    fontSize: 20,
+    paddingTop: 75,
+  },
+  buttonContainer: {
+    paddingBottom: 90,
+  },
+  button: {
+    padding: 15,
+    borderRadius: 75,
+    backgroundColor: '#FF0000',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  buttonText: {
+    color: '#FFF',
+    fontSize: 20,
   },
 });
